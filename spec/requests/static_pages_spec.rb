fc51e63e5676566d1ac6c68bdcb90de5123c5c97
vladimirov-k-a2015 <sheyan.anton@gmail.com>
2016-11-03 15:11:01 +0200

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'App20161103'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('App20161103')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial App20161103 | Home")
+    end
+
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial App20161103 | Help")
+    end
+
+  end
+
+  describe "About page" do
+
+    it "should have the content 'О приложении'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('О приложении')
+    end
+
+
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial App20161103 | About")
+    end
+
+  end
+
+end
